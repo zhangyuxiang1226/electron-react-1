9e9587bbc5f0f3080c7f8994b7775c2e9ad320b4
yuxiang.zhang <yuxiang.zhang@ubtrobot.com>
2020-03-10 17:03:53 +0800

    实现windows环境打包

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## 启动
 `npm run start`
 
-## 构建
+## 打包
+`npm run build:win`
 
 ### 参考：
 https://juejin.im/post/5b86b7fd6fb9a019c476fc06
--- app/index.js
+++ app/index.js
@@ -1,6 +1,30 @@
 const { app, BrowserWindow } = require('electron');
-
+const path = require('path');
 let win;
+
+// 开发工具
+const isDev = require('electron-is-dev')
+// electron-debug
+isDev && require('electron-debug')({
+    enable: true,
+    showDevTools: false,
+})
+// Chromium
+function createDevTools() {
+    const { default: installExtension, REACT_DEVELOPER_TOOLS, MOBX_DEVTOOLS } = require('electron-devtools-installer')
+    // devtron
+    const devtronExtension = require('devtron');
+    devtronExtension.install();
+    // react工具
+    installExtension(REACT_DEVELOPER_TOOLS);
+    installExtension(MOBX_DEVTOOLS);
+}
+
+// 开发环境地址
+const devUrl = 'http://localhost:3000'
+// 本地文件路径定位到打包的react文件
+const localUrl = `file://${path.resolve(__dirname, '../../app.asar/build')}/index.html`;
+const appUrl = isDev ? devUrl : localUrl;
 
 function createWindow() {
     // 创建浏览器窗口。
@@ -11,50 +35,24 @@ function createWindow() {
             nodeIntegration: false
         }
     });
-
     // 然后加载应用的 index.html。
-    win.loadURL('http://localhost:3000');
-
-    // 当 window 被关闭，这个事件会被触发。
+    win.loadURL(appUrl);
     win.on('closed', () => {
         win = null;
     });
 }
-
-// 开发工具
-const isDev = require('electron-is-dev')
-
-// electron-debug
-isDev && require('electron-debug')({
-    enable: true,
-    showDevTools: false,
-})
-
-// Chromium
-function createDevTools() {
-    const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, MOBX_DEVTOOLS } = require('electron-devtools-installer')
-    // devtron
-    const devtronExtension = require('devtron');
-    devtronExtension.install();
-    // react工具
-    installExtension(REACT_DEVELOPER_TOOLS);
-    installExtension(MOBX_DEVTOOLS);
-}
-
 
 app.on('ready', () => {
     createWindow();
     // 开发环境启动开发工具
     isDev && createDevTools();
 });
-
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
 app.on('activate', () => {
     if (win === null) {
         createWindow();
